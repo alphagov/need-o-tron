677a80bf11323f94b2bb17799c31e254f3cde8d8
James Stewart <jys@ketlai.co.uk>
2011-06-09 16:44:23 +0100

    Set email domain for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,4 +51,5 @@ NeedOTron::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.action_mailer.default_url_options = { :host => 'needotron.alpha.gov.uk' }
 end
