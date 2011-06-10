6a154431361171046e7b1298bccc4210b58a74fe
James Stewart <jys@ketlai.co.uk>
2011-06-10 09:33:02 +0100

    Accept nested attributes for needs->justifications Only show justification section if this is a new record

--- app/models/need.rb
+++ app/models/need.rb
@@ -3,5 +3,6 @@ class Need < ActiveRecord::Base
   has_and_belongs_to_many :departments
   has_many :justifications
   
+  accepts_nested_attributes_for :justifications, :reject_if => :all_blank
   acts_as_taggable
 end
