bbe0f514070ee37375f9a45654d9467e1587157b
James Stewart <jys@ketlai.co.uk>
2011-06-13 10:34:24 +0100

    Store the current user in the thread to allow us to capture that in various models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!, :unless => :devise_controller?
+  before_filter :set_current_user_in_thread, :if => :user_signed_in?
   
   protected
+    def set_current_user_in_thread
+      Thread.current[:current_user] = current_user
+    end
+  
     def authenticate_inviter!
       authenticate_user! and current_user.role == 'superuser'
     end
