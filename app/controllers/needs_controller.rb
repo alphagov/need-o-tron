57b2e3953f8b03fd4dbed9c8f7e49c101f89b9fe
James Stewart <jys@ketlai.co.uk>
2011-06-17 16:00:17 +0100

    Show error messages in the context of the form you were on

--- app/controllers/needs_controller.rb
+++ app/controllers/needs_controller.rb
@@ -1,3 +1,17 @@
 class NeedsController < InheritedResources::Base
   has_scope :in_state
+  
+  def update
+    update! do |success, failure|
+      failure.html { 
+        if request.referer.match(/needs\/\d+/)
+          @need.reload
+          flash.now[:alert] = "We weren't able to save your changes. Please review the form below and try again"
+          render :action => 'show'
+        else
+          render :action => 'edit'
+        end
+      }
+    end
+  end
 end
