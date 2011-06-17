2f7a6e7101e11793e65a9d1abf59ea4497cf20e2
James Stewart <jys@ketlai.co.uk>
2011-06-17 13:54:43 +0100

    Add evidence type seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 audiences = ['Everyone', 'Businesses', 'Professionals', 'Politicians/Govt', 'Parents', 'Carers', 'Young people', 'Disabled people', 'Overseas', 'Older people']
 audiences.each { |a| Audience.find_or_create_by_name(a) }
+
+["Search behaviour", "User request", "Legal framework", "Govt obligation", "Official channel", "Govt campaign"].each do |et_name|
+  EvidenceType.find_or_create_by_name(et_name)
+end
