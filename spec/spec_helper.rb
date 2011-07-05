d51b7154619fc9a7c0e8867c7d68664429a67c48
Matt Patterson <matt@reprocessed.org>
2011-07-05 15:55:27 +0100

    Remove last vestiges of Devise

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,7 +24,4 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-  
-  config.include Devise::TestHelpers, :type => :controller
-  config.extend DeviseMacros, :type => :controller
 end
--- spec/support/devise_macros.rb
+++
@@ -1,9 +0,0 @@
-module DeviseMacros
-  def login_user
-    before(:each) do
-      @request.env["devise.mapping"] = Devise.mappings[:user]
-      @user = User.new
-      sign_in @user
-    end
-  end
-end
