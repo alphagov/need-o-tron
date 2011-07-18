766130f7db72a0a56a85aad4033106abf7b88b37
James Stewart <jys@ketlai.co.uk>
2011-07-18 16:55:34 +0100

    Include extra fields when exporting CSV

--- lib/needs_csv.rb
+++ lib/needs_csv.rb
@@ -4,9 +4,10 @@ require 'csv'
 class NeedsCsv < CsvRenderer
   def to_csv
     CSV.generate do |csv|
-      csv << ["Id", "Title", "Context", "Tags", "Status", "Audiences", "Updated at"]
+      csv << ["Id", "Title", "Context", "Tags", "Status", "Audiences", "Updated at", "Statutory", "Search rank", "Pairwise rank", "Traffic", "Usage volume", "Interaction", "Related needs"]
       @data.each do |need|
-        csv << [need.id, need.title, need.description, need.tag_list, need.status, need.audiences.collect { |a| a.name }.join(", "), need.updated_at.to_formatted_s(:db)]
+        csv << [need.id, need.title, need.description, need.tag_list, need.status, need.audiences.collect { |a| a.name }.join(", "), need.updated_at.to_formatted_s(:db), 
+          need.statutory, need.search_rank, need.pairwise_rank, need.traffic, need.usage_volume, need.interaction, need.related_needs]
       end
     end
   end
