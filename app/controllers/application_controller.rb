546d3209e4f90531780c06b7a651548094bd1516
Matt Patterson <matt@reprocessed.org>
2011-09-05 16:08:52 +0100

    Stop our requiring-admins policy preventing anyone from ever logging in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
   before_filter :set_current_user_in_thread, :if => :user_signed_in?
-  before_filter :ensure_user_is_admin!, :except => [:show, :create, :index, :new]
+  # the user_signed_in? if here is needed becaue otherwise we require an admin user
+  # on the OmniAuth callback controller action, which is exceeding lethal
+  before_filter :ensure_user_is_admin!, :except => [:show, :create, :index, :new], :if => :user_signed_in?
 
   def ensure_user_is_admin!
     unless current_user.is_admin?
