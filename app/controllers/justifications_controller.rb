352295321ea38c2eed2729cdd89bffd813d15e17
James Stewart <jys@ketlai.co.uk>
2011-09-06 09:18:01 +0100

    It turns out that since version 1.2 inherited_resources will do all the redirects we were overriding automatically based on a single declaration. So let's remove lots of unnecessary code!
    ( see "Smart redirects" under https://github.com/josevalim/inherited_resources )

--- app/controllers/justifications_controller.rb
+++ app/controllers/justifications_controller.rb
@@ -1,19 +1,4 @@
 class JustificationsController < InheritedResources::Base
   belongs_to :need
-  
-  def index
-    redirect_to @need
-  end
-  
-  def create
-    create! { @need }
-  end
-  
-  def update
-    update! { @need }
-  end
-  
-  def destroy
-    destroy! { @need }
-  end
+  actions :all, :except => [:show, :index]
 end
