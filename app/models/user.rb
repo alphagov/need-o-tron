ac8efc6e218bbc44e813aba2fd7a8583b37fe1c6
Matt Patterson <matt@reprocessed.org>
2011-09-09 11:04:23 +0100

    Make the gds-sso user-finder actually look at the right things

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,17 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :uid, :version
 
   def self.find_for_gds_oauth(auth_hash)
-    logger.info "AUTH_HASH: #{auth_hash.inspect}"
-    if user = User.find_by_email(auth_hash["uid"])
+    user = find_by_uid(auth_hash["uid"])
+    if user
       user
-    else # Create a user with a stub password. 
-      user_params = auth_hash.dup.keep_if { |k,v| ['uid', 'email', 'name', 'version'].include?(k) }
-      User.create!(user_params) 
+    else # Create a user.
+      create_from_auth_hash(auth_hash)
     end
+  end
+
+  def self.create_from_auth_hash(auth_hash)
+    user_params = auth_hash['extra']['user_hash'].dup.keep_if { |k,v| ['uid', 'email', 'name', 'version'].include?(k) }
+    User.create!(user_params)
   end
 
   def to_s
