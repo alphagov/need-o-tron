d9e8a84f71ffe965a03fa5924f12e8655622b7cc
Craig R Webster <craig@barkingiguana.com>
2011-10-07 15:37:18 +0100

    Allow assignment of work to others

--- app/helpers/needs_helper.rb
+++ app/helpers/needs_helper.rb
@@ -11,4 +11,21 @@ module NeedsHelper
       submit_tag('Start work on this need', :disable_with => 'Working...', :class => 'fulfill').html_safe
     end.html_safe
   end
+
+  def assign_work_link need
+    start_work =  "#{Plek.current.find("arbiter")}/artefacts"
+    start_work += "?return_to=#{Plek.current.find("needs")}#{request.fullpath}"
+
+    form_tag start_work, :method => 'post' do
+      [
+        hidden_field_tag("artefact[name]", "[#{need.id}] I need to be changed").html_safe,
+        hidden_field_tag("artefact[slug]", "#{need.id}-i-need-to-be-changed").html_safe,
+        hidden_field_tag("artefact[owning_app]", "publisher").html_safe,
+        hidden_field_tag("artefact[need_id]", need.id).html_safe,
+        hidden_field_tag("artefact[kind]", need.kind.to_s.downcase).html_safe,
+        hidden_field_tag("artefact[tags]", need.tag_list).html_safe
+      ].join.html_safe + \
+      submit_tag('Assign need', :disable_with => 'Working...', :class => 'fulfill').html_safe
+    end.html_safe
+  end
 end
