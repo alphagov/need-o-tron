554fbf08d1e3cb833f4650198c2ffcf141b627bc
Jordan Hatch <jordan@jordanh.net>
2011-11-15 17:51:19 +0000

    Updated spec tests for new listener

--- spec/unit/need_state_listener_spec.rb
+++ spec/unit/need_state_listener_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe SatisfiedNeedListener do
+describe NeedStateListener do
   def panopticon_will_respond_with(metadata)
     stub_request(:get, %r{http://panopticon.*/artefacts/.*\.js}).
       to_return(:status => 200, :body => metadata.to_json)
@@ -13,10 +13,10 @@ describe SatisfiedNeedListener do
       body: {answer:{panopticon_id: "any_old_thing"}}.to_xml,
       headers: {'message-id' => '123'}
     )
-    stomp_client = stub(join: nil, close: nil, acknowledge: nil)
+    stomp_client = stub(join: nil, close: nil, acknowledge: nil, subscribe: nil)
     stomp_client.expects(:subscribe).yields(need_satisfied_message)
-    SatisfiedNeedListener.client = stomp_client
-    SatisfiedNeedListener.new.listen
+    NeedStateListener.client = stomp_client
+    NeedStateListener.new.listen
     need.reload
     need.should be_done
     need.url.should =~ %r{/my_slug$}
