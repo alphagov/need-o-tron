e7c1cd0c101c73290011a62114949cfe3e134be8
Craig R Webster <craig@barkingiguana.com>
2011-11-21 13:04:04 +0000

    Whitespace

--- app/controllers/needs_controller.rb
+++ app/controllers/needs_controller.rb
@@ -6,17 +6,17 @@ class NeedsController < InheritedResources::Base
 
   def index
     index! do |format|
-      format.csv { render :csv => NeedsCsv.new(collection, Time.zone.now) } 
+      format.csv { render :csv => NeedsCsv.new(collection, Time.zone.now) }
     end
-  end                                                                       
-  
+  end
+
   def show
     show! do |format|
       format.json { # show.json.rabl
         }
     end
   end
-  
+
   def print
     @need = Need.find params[:id]
     render :layout => false
