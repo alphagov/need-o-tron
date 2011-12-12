1564231576802d13b6afead62d443aea26db755e
James Stewart <jys@ketlai.co.uk>
2011-12-12 11:48:45 +0000

    Add extra logging to try and work out why test fails on jenkins but not locally

--- spec/unit/need_state_listener_spec.rb
+++ spec/unit/need_state_listener_spec.rb
@@ -9,12 +9,16 @@ describe NeedStateListener do
 
   it 'marks a need as done and records the public url when a message received' do
     need = FactoryGirl.create(:need, :indexer => NullIndexer)
-    panopticon_has_metadata('id' => 12345, 'need_id' => need.id, 'slug' => 'my_slug')
+    Rails.logger.warn "NSL TEST: Created need: #{need.inspect}"
+    panopticon_url = panopticon_has_metadata('id' => 12345, 'need_id' => need.id, 'slug' => 'my_slug')
+    Rails.logger.warn "NSL TEST: Created in panopticon: #{panopticon_url}"
 
     listener = NeedStateListener.new
     listener.act_on_published({'panopticon_id' => 12345})
+    Rails.logger.warn "NSL TEST: Acted on panopticon - Need is now #{need.status}"
     
     need.reload
+    Rails.logger.warn "NSL TEST: Reloaded need: #{need.inspect}"
     need.should be_done
     need.url.should =~ %r{/my_slug$}
   end
