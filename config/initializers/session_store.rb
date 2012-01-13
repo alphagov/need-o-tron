3166b2c0279253a6d522308a1c74518c949c6829
James Stewart <jys@ketlai.co.uk>
2012-01-13 12:52:05 +0000

    Set cookies to only be served over https and to be http only

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
-NeedOTron::Application.config.session_store :cookie_store, key: '_need-o-tron_session'
+NeedOTron::Application.config.session_store :cookie_store,
+  key: '_need-o-tron_session',
+  :secure => Rails.env.production?,
+  :http_only => true
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
