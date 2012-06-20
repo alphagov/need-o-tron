38bab39a105c9dc0b2f4d2dcd6eb5b6bf98a7266
James Stewart <jys@ketlai.co.uk>
2012-06-20 15:58:46 +0100

    Include URL in need-o-tron export

--- lib/needs_csv.rb
+++ lib/needs_csv.rb
@@ -12,11 +12,13 @@ class NeedsCsv < CsvRenderer
 
   def to_csv
     CSV.generate do |csv|
-      csv << ["Id", "Lead department", "Priority", "Title", "Format", "Tags", "Context", "Status", "Updated at", "Statutory", "Fact checker", "Writing dept", "Interaction", "Related needs", "Reason for formatting decision", "Reason for decision", "DG Links", "Existing Services"]
+      csv << ["Id", "Lead department", "Priority", "Title", "URL", "Format", "Tags", "Context", "Status", "Updated at", "Statutory", "Fact checker", "Writing dept", "Interaction", "Related needs", "Reason for formatting decision", "Reason for decision", "DG Links", "Existing Services"]
       @data.each do |need|
-        csv << [need.id, accountabilities(need), need.named_priority, need.title, need.kind.to_s, need.tag_list,
-                need.description, need.status, need.updated_at.to_formatted_s(:db),
-                need.statutory, fact_checkers(need), need.writing_department.to_s,
+        csv << [need.id, accountabilities(need), need.named_priority, need.title, 
+                (need.url || "").gsub('www.production.alphagov.co.uk', 'www.gov.uk'), 
+                need.kind.to_s, need.tag_list, need.description, need.status,
+                need.updated_at.to_formatted_s(:db), need.statutory, 
+                fact_checkers(need), need.writing_department.to_s, 
                 need.interaction, need.related_needs, need.reason_for_formatting_decision,
                 need.reason_for_decision, need.directgov_links.collect(&:directgov_id).join(','),
                 need.existing_services.collect(&:link).join(',')
