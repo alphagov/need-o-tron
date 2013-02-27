6f77d19d83186c962c9b7eac3f2d1749fea5b9b1
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-02-27 22:33:30 +0000

    Redirect daily report emails This commit moves the annoying daily emails which everyone except @jystewart filters to the bin into an opt-in list rather than govuk-dev and govuk-team

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -10,9 +10,9 @@ class ReportMailer < ActionMailer::Base
 
     case Plek.current.environment
     when 'preview'
-      email_address = 'govuk-dev@digital.cabinet-office.gov.uk'
+      email_address = 'needotron-daily+preview@digital.cabinet-office.gov.uk'
     else
-      email_address = 'govuk-team@digital.cabinet-office.gov.uk'
+      email_address = 'needotron-daily+production@digital.cabinet-office.gov.uk'
     end
 
     mail(:to => email_address, :subject => analytics_subject(daily, weekly))
