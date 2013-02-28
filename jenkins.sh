40e09019bf129bd4e15b24ae898f31644d55e742
Mazz <mazz.mosley@digital.cabinet-office.gov.uk>
2013-02-28 10:55:21 +0000

    Run migrations too

--- jenkins.sh
+++ jenkins.sh
@@ -3,6 +3,6 @@
 export RAILS_ENV=test
 
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
-bundle exec rake db:drop db:create db:schema:load
+bundle exec rake db:drop db:create db:schema:load db:migrate
 bundle exec rake stats
 FACTER_govuk_platform=test bundle exec rake ci:setup:rspec spec
