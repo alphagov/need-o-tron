2ab82ee41d2dbf4fae7f5092b92ce2526aeeeb26
Mazz <mazz.mosley@digital.cabinet-office.gov.uk>
2013-02-28 11:17:00 +0000

    Configure tire properly

--- config/initializers/tire.rb
+++ config/initializers/tire.rb
@@ -1,4 +1,16 @@
-Tire.configure {
-  url "http://support.cluster:9200"
-  logger Rails.root.join("log/tire_#{Rails.env}.log")
-}
+if Rails.env.production?
+  Tire.configure {
+  	url "http://support.cluster:9200"
+    logger Rails.root.join("log/tire_#{Rails.env}.log")
+  }
+elsif Rails.env.development?
+  Tire.configure {
+    url "http://localhost:9200"
+    logger Rails.root.join("log/tire_#{Rails.env}.log")
+  }
+else
+  Tire.configure {
+    url "http://localhost:9200"
+    logger Rails.root.join("log/tire_#{Rails.env}.log")
+  }
+end
