4d4502e58eee9063f9b98ffa2897625268bd785a
Mazz <mazz.mosley@digital.cabinet-office.gov.uk>
2013-02-28 11:38:50 +0000

    Don't need the development conditional now

--- config/initializers/tire.rb
+++ config/initializers/tire.rb
@@ -3,11 +3,6 @@ if Rails.env.production?
   	url "http://support.cluster:9200"
     logger Rails.root.join("log/tire_#{Rails.env}.log")
   }
-elsif Rails.env.development?
-  Tire.configure {
-    url "http://localhost:9200"
-    logger Rails.root.join("log/tire_#{Rails.env}.log")
-  }
 else
   Tire.configure {
     url "http://localhost:9200"
