01f85da38266515a2f786a35808e56548d19a653
Jordan Hatch <jordan@jordanh.net>
2013-04-03 10:29:38 +0100

    Combine sources into a single table
    This moves Directgov links, existing services and
    justifications into the Sources table instead.
    Their kinds are carried across too.
    
    This migration is reversible, with the exception
    of the Directgov link titles as Directgov can no
    longer be reached to retrieve these.

---
+++ db/migrate/20130402160312_change_directgov_links_to_sources.rb
@@ -0,0 +1,23 @@
+class ChangeDirectgovLinksToSources < ActiveRecord::Migration
+  def up
+    DirectgovLink.all.each do |link|
+      Source.create!(
+        :title => link.title,
+        :url => link.url,
+        :kind => 'directgov',
+        :need_id => link.need_id
+      )
+      link.destroy
+    end
+  end
+
+  def down
+    Source.where(:kind => 'directgov').all.each do |source|
+      DirectgovLink.create!(
+        :directgov_id => source.url.match(%r{http://www.direct.gov.uk/en/(.*)}) {|m| m[1] },
+        :need_id => source.need_id
+      )
+      source.destroy
+    end
+  end
+end
